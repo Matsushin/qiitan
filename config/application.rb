75245e9bb21d02e859536dcc0af54f2a81ef8961
matsushin <matsu1983@gmail.com>
2018-10-30 22:32:46 +0900

    見た目を調整

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Qiitan
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.i18n.default_locale = :ja
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
