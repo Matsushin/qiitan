b1aa5a53e7eaaa17f7fc23ee76cf791125cad24e
matsushin11 <matsu1983@gmail.com>
2018-10-31 12:17:54 +0900

    ユーザ登録関連の見た目を調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :articles, dependent: :destroy
+
+  validates :username, presence: true
+  validates :email, presence: true
 end
