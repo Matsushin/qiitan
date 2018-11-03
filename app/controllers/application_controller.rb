5b2a18c560e165138b00ed5d992fd7b69b96b581
matsushin <matsu1983@gmail.com>
2018-11-03 17:26:15 +0900

    微修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_query
 
   protected
 
@@ -9,5 +10,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+  end
+
+  private
+
+  def set_query
+    @q =Article.none.search(params[:q])
   end
 end
