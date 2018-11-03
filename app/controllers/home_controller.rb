685cd3408562e4e8f3c9dd25f6bb72c335f4374b
matsushin <matsu1983@gmail.com>
2018-11-03 17:23:20 +0900

    記事を検索できるように

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @articles = Article.includes(:user).order(created_at: :desc).page(params[:page])
+    @q = Article.includes(:user).order(created_at: :desc).search(params[:q])
+    @articles = @q.result.page(params[:page])
   end
 end
