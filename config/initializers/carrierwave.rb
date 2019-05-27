299230db38edf132c02015b79bc435167f855732
matsushin <matsu1983@gmail.com>
2019-05-27 22:33:59 +0900

    微修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,7 +8,7 @@ else
     config.fog_credentials = {
         provider:               'AWS',
         aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
-        aws_secret_access_key:  ENV['AWS_SECRET_KEY_ID'],
+        aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
         region:                 'ap-northeast-1'
     }
     config.storage = :fog
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,7 +12,6 @@ if [ -n "$PARAMETER_STORE_PREFIX" ]; then
     export SENDGRID_USERNAME=$(aws ssm get-parameters --name /qiitan/stg/SENDGRID_USERNAME --with-decryption --query "Parameters[0].Value" --region ap-northeast-1 --output text)
     export SENDGRID_PASSWORD=$(aws ssm get-parameters --name /qiitan/stg/SENDGRID_PASSWORD --with-decryption --query "Parameters[0].Value" --region ap-northeast-1 --output text)
     export AWS_ACCESS_KEY_ID=$(aws ssm get-parameters --name /qiiatan/stg/aws_access_key_id --with-decryption --query "Parameters[0].Value" --region ap-northeast-1 --output text)
-    export AWS_SECRET_KEY_ID=$(aws ssm get-parameters --name /qiiatan/stg/aws_secret_access_key --with-decryption --query "Parameters[0].Value" --region ap-northeast-1 --output text)
     export AWS_SECRET_ACCESS_KEY=$(aws ssm get-parameters --name /qiiatan/stg/aws_secret_access_key --with-decryption --query "Parameters[0].Value" --region ap-northeast-1 --output text)
 fi
 
