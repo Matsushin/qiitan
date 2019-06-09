7364cd809f5c9f80f75c98b789d75a927d460d84
matsushin <matsu1983@gmail.com>
2019-06-09 23:55:37 +0900

    微修正

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,6 @@
 eval $(aws ecr get-login --no-include-email --region ap-northeast-1)
 docker push $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/qiitan/stg:latest
 
-
 cat << EOF > run_task_db_migrate.json
 {
   "containerOverrides": [
