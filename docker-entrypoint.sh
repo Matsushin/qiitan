e16d280c16db7bc38f11e7071be1fe55f818e6cd
matsushin <matsu1983@gmail.com>
2019-06-14 00:11:37 +0900

    AWS情報の取得をECS側に移行

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,8 +11,6 @@ if [ -n "$PARAMETER_STORE_PREFIX" ]; then
     export SECRET_KEY_BASE=$(aws ssm get-parameters --name /qiitan/stg/SECRET_KEY_BASE --with-decryption --query "Parameters[0].Value" --region ap-northeast-1 --output text)
     export SENDGRID_USERNAME=$(aws ssm get-parameters --name /qiitan/stg/SENDGRID_USERNAME --with-decryption --query "Parameters[0].Value" --region ap-northeast-1 --output text)
     export SENDGRID_PASSWORD=$(aws ssm get-parameters --name /qiitan/stg/SENDGRID_PASSWORD --with-decryption --query "Parameters[0].Value" --region ap-northeast-1 --output text)
-    export AWS_ACCESS_KEY_ID=$(aws ssm get-parameters --name /qiitan/stg/aws_access_key_id --with-decryption --query "Parameters[0].Value" --region ap-northeast-1 --output text)
-    export AWS_SECRET_ACCESS_KEY=$(aws ssm get-parameters --name /qiitan/stg/aws_secret_access_key --with-decryption --query "Parameters[0].Value" --region ap-northeast-1 --output text)
 fi
 
 exec "$@"
