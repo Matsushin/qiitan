65a8cabc5119640526dda31bca6f0925853bb24f
matsushin <matsu1983@gmail.com>
2019-06-15 12:12:02 +0900

    RSpecが通るように

--- spec/system/article_spec.rb
+++ spec/system/article_spec.rb
@@ -1,22 +1,13 @@
 describe '投稿記事管理機能', type: :system do
+  let(:user) { create(:user, email: 'test@example.com', username: 'test_user', confirmed_at: Time.current) }
   before do
-    @user = User.create(
-      username: "test",
-      email: "test@example.com",
-      password: "password",
-      confirmed_at: Time.current
-    )
-    @user.password = "password"
-
-    @user.save
+    user.password = "password"
+    user.save
   end
   it '投稿削除テスト' do
     visit new_user_session_path
-    p @user.email
-    p @user.authenticate? "password"
-    p @user.authenticate?("password").to_s
-    p "###################################"
-    fill_in 'メールアドレス', with: @user.email
+
+    fill_in 'メールアドレス', with: "test@example.com"
     fill_in 'パスワード', with: "password"
     click_button "Qiitan にログイン"
     expect(current_path).to eq root_path
@@ -26,10 +17,12 @@ describe '投稿記事管理機能', type: :system do
     fill_in "article_title", with: "Test"
     fill_in "article_body", with: "body"
     click_button "Qiitanに投稿"
+    expect(current_path).to eq article_path(Article.last)
     expect(page).to have_content 'Test'
-    click_link "Test"
-    click_button "削除する"
-    page.accept_confirm '削除してもよろしいですか？'
+
+    click_link "削除する"
+    expect(current_path).to eq root_path
     expect(page).to have_no_content "Test"
+    expect(Article.count).to be_zero
   end
 end
