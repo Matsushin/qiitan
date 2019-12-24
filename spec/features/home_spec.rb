3bd7fe0baa21a8fe037ec1dd8bbe5a9791d9742a
hiroki <1112hiroki.suzuki@gmail.com>
2019-12-24 15:08:12 +0900

    sass/Rspec修正・加筆

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -2,25 +2,34 @@ require 'rails_helper'
 
 feature 'Home' do
   background { login_as user, scope: :user }
-  context 'Home User Window' do
-    let(:user) { create(:user, email: 'yamada@qiitan.jp', username: 'yamada', confirmed_at: Time.current) }
+  let!(:article1) { create(:article, user: user, title: '記事タイトル1 home_spec確認', body: '記事本文1') }
+  let!(:article2) { create(:article, user: user, title: '記事タイトル2', body: '記事本文2') }
 
-    scenario 'HOME画面から自分の「ユーザー名」からユーザー詳細画面に遷移できる', js: true do
+  context 'Home User Container' do
+    let(:user) { create(:user, email: 'test@example.com', username: 'test_user', confirmed_at: Time.current) }
+
+    scenario 'HOME画面の自分の「Items(記事数)」の表示の確認', js: true do
       visit root_path
-      expect(page).to have_content 'Items'
-
-      find(".h4").click
-      expect(page).to have_content "最近の投稿"
-
+      expect(page).to have_selector '.articles-count', text: '2'
     end
 
-    scenario 'HOME画面から自分の「投稿記事数」からユーザー詳細画面に遷移できる', js: true do
+    scenario 'HOME画面の自分の「Contributtion(貢献数)」の表示の確認', js: true do
+      visit root_path
+      expect(page).to have_selector '.contributtion-count', text: '0'
+    end
+
+    scenario 'HOME画面の自分の「ユーザー名」からユーザー詳細画面に遷移できる', js: true do
       visit root_path
       expect(page).to have_content 'Items'
+      find(".user-name").click
+      expect(page).to have_content "記事タイトル1 home_spec確認"
+    end
 
-      find(".a").click
-      expect(page).to have_content "最近の投稿"
-
+    scenario 'HOME画面の自分の「投稿記事数」からユーザー詳細画面に遷移できる', js: true do
+      visit root_path
+      expect(page).to have_content 'Items'
+      find(".articles-count").click
+      expect(page).to have_content "記事タイトル1 home_spec確認"
     end
   end
 end
