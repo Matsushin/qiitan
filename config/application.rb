44819af48b687d63ae71136abd8e73439dc27efd
matsushin <matsu1983@gmail.com>
2020-01-19 23:08:08 +0900

    アップデートタスクを実行

--- config/application.rb
+++ config/application.rb
@@ -5,12 +5,15 @@ require "rails"
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
+# require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -31,5 +34,8 @@ module Qiitan
     config.generators.system_tests = nil
     config.rack_dev_mark.enable = !Rails.env.production?
     config.rack_dev_mark.theme = [:title, Rack::DevMark::Theme::GithubForkRibbon.new(position: 'right')]
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
