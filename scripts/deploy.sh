58a821e19cbfc15af13be825486bca17afede742
matsushin <matsu1983@gmail.com>
2020-06-28 09:51:59 +0900

    デプロイするサービス名を変更

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -23,6 +23,6 @@ aws ecs run-task \
 
 ./scripts/ecs-deploy \
   --cluster qiitan-cluster-stg \
-  --service-name qiitan-service-stg-20200124 \
+  --service-name qiitan-service-stg \
   --image $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/qiitan/stg:latest \
   --timeout 600
