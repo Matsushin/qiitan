75736a38cae9676fdbd777f1f4363f038a3e0341
nishimura55 <naoki.hnmr55@gmail.com>
2020-07-05 19:01:20 +0900

    homeコントローラーを修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
 
   def index
-    @articles = Article.includes(:user).order(created_at: :desc).page(params[:page])
+    # @articles = Article.includes(:user).order(created_at: :desc).page(params[:page])
+    @articles = Article.order(created_at: :desc).page(params[:page])
   end
 end
